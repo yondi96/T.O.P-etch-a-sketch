b93ad2e74c6e362b3bba99298c59dcd2bf1e5b52
yondi96 <bautista.dioncarlo@gmail.com>
2021-12-21 20:36:16 +0800

    added 16x16 boxes

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 // * an app similar to sketchpad and an etch a sketch
+
+// create a div
+
+
+for(i = 0; i <= 256; i++) {
+  const div = document.createElement("div");
+
+  div.style.height = "40px";
+  div.style.width = "40px";
+  div.style.background = "red";
+  div.style.border = "1px solid black"
+
+  div.classList.add("test");
+
+  document.querySelector(".grid-container").appendChild(div);
+}
